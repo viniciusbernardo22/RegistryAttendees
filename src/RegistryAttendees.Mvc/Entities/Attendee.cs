f256cced8760f404d58fb5c25646e4721281dedb
viniciusbernardo22 <vini383@gmail.com>
2024-07-09 11:01:31 -0300

    registering props on entity model

---
+++ src/RegistryAttendees.Mvc/Entities/Attendee.cs
@@ -0,0 +1,18 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using RegistryAttendees.Mvc.Models;
+
+namespace RegistryAttendees.Mvc.Entities;
+
+public class Attendee : ITableEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; private set; }
+    
+    /* Azure TbEntity Props */
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
