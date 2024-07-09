0c03058067649b44adc85b1de37fa97e0dce0157
viniciusbernardo22 <vini383@gmail.com>
2024-07-09 11:01:13 -0300

    feat: implementing abstraction on implementation service

---
+++ src/RegistryAttendees.Mvc/Services/TableStorageService.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Data.Tables;
+using RegistryAttendees.Mvc.Interfaces;
+
+namespace RegistryAttendees.Mvc.Services;
+
+public class TableStorageService<T> : ITableStorageService<T>
+{
+    private const string TableName = $"{nameof(T)}s";
+    
+    private readonly IConfiguration _configuration;
+
+    private async Task<TableClient> GetTableClient()
+    {
+        var serviceClient = new TableServiceClient(_configuration["StorageConnectionString"]);
+        var tableClient = serviceClient.GetTableClient(TableName);
+        /*Create table if not exists*/
+        await tableClient.CreateIfNotExistsAsync();
+        return tableClient;
+    }
+    
+    public TableStorageService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task<T> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<T>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Upsert(T attendee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete(string? id)
+    {
+        throw new NotImplementedException();
+    }
+}
