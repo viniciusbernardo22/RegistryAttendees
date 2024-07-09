1e9a41d34fddd94314190461915e8f6b75636f7d
viniciusbernardo22 <vini383@gmail.com>
2024-07-09 11:00:55 -0300

    feat: implementation of abstraction

---
+++ src/RegistryAttendees.Mvc/Interfaces/ITableStorageService.cs
@@ -0,0 +1,15 @@
+﻿using RegistryAttendees.Mvc.Entities;
+
+namespace RegistryAttendees.Mvc.Interfaces;
+
+public interface ITableStorageService<T>
+{
+        Task<T> GetById(string id);
+
+        Task<List<T>> GetAll();
+
+        Task Upsert(T attendee);
+
+        Task Delete(string? id);
+
+}
